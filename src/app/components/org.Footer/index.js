db562cbbff7bb48db2e5b0ee9272f71bec437c65
Michael Anderson <normanganderson@gmail.com>
2015-09-10 22:47:04 -0600

    All the stolen stuff

---
+++ src/app/components/org.Footer/index.js
@@ -0,0 +1,40 @@
+import style from './style'
+import React from 'react'
+import CSSModules from 'react-css-modules'
+import Flexbox from 'obj.Flexbox'
+import { Link } from 'react-router'
+import Router from 'utils.routing'
+
+const links = [
+
+];
+
+const Nav = React.createClass({
+
+  renderNavItem (item, i) {
+    return (
+      <li styleName='item' key={i}>
+        <Link styleName='link' to={item.to}>
+          { item.name }
+        </Link>
+      </li>
+    )
+  },
+
+  render () {
+
+    return (
+      <Flexbox tag='header' styleName='root' justify='center'>
+
+        <Flexbox tag='ul' justify='space-between'>
+
+          {links.map(this.renderNavItem)}
+
+        </Flexbox>
+      </Flexbox>
+    )
+  }
+
+})
+
+export default CSSModules(Nav, style)
---
+++ src/app/components/org.Nav/index.js
@@ -0,0 +1,47 @@
+import style from './style'
+import React from 'react'
+import { Link } from 'react-router'
+import Router from 'utils.routing'
+import CSSModules from 'react-css-modules'
+import Flexbox from 'obj.Flexbox'
+
+const links = [
+  {
+    name: 'Tracked Activities',
+    to: 'wildPokemon'
+  },
+  {
+    name: 'New Tracker',
+    to: 'simpleForm'
+  }
+]
+
+const Nav = React.createClass({
+
+  renderNavItem (item, i) {
+    return (
+      <li styleName='item' key={i}>
+        <Link styleName='link' to={item.to}>
+          { item.name }
+        </Link>
+      </li>
+    )
+  },
+
+  render () {
+
+    return (
+      <Flexbox tag='header' styleName='container' justify='center'>
+
+        <Flexbox tag='ul' justify='space-between'>
+
+          {links.map(this.renderNavItem)}
+
+        </Flexbox>
+      </Flexbox>
+    )
+  }
+})
+
+export default CSSModules(Nav, style)
+
